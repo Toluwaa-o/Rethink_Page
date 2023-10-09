9cb27570c94db89c552dc940fd0846b8814396c8
Toluwalashe <asheogunleye02@gmail.com>
2023-10-09 11:20:33 +0100

    Search state handler

--- src/Components/Search Bar/InputField.js
+++ src/Components/Search Bar/InputField.js
@@ -1,4 +1,4 @@
-export default function InputField() {
+export default function InputField({ data, clickHandler }) {
   return (
     <input
       className="placeholder:text-[#463D61] placeholder:text-sm placeholder:tracking-wider w-[90%] outline-none"
@@ -6,6 +6,8 @@ export default function InputField() {
       name="Search"
       aria-label="Search"
       placeholder="Search for templates, projects, etc"
+      value={data}
+      onChange={(e) => clickHandler(e.target.value)}
     />
   );
 }
--- src/Components/Search Bar/SearchBar.js
+++ src/Components/Search Bar/SearchBar.js
@@ -1,17 +1,22 @@
+import { memo, useState } from "react";
 import Clear from "./Icons/Clear";
 import Filter from "./Icons/Filter";
 import Search from "./Icons/Search";
 import InputField from "./InputField";
 
-export default function SearchBar() {
+function SearchBar() {
+  const [searchData, setSearchData] = useState("");
+
   return (
     <div className="flex m-auto w-[90%] md:w-[35vw] md:m-[unset] bg-white p-3 rounded-md items-center gap-2 relative top-3 md:top-[unset] border border[#EBEAEF] hover:border-[#754DE8] hover:fill-[#754DE8] fill-gray-300 transition-all">
       <Search />
-      <InputField />
+      <InputField data={searchData} clickHandler={setSearchData} />
       <span className="hidden md:flex md:gap-4 md:items-center">
         <Filter />
-        <Clear />
+        <Clear clickHandler={() => setSearchData("")} />
       </span>
     </div>
   );
 }
+
+export default memo(SearchBar);
